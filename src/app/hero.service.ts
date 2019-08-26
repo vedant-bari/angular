afe5fd247d6ca709459b893f02e1f16e54b228de
akshay <vedantbari40@gmail.com>
2019-08-26 18:27:10 +0530

    latest

---
+++ src/app/hero.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor() { }
+  getHeroes(): Hero[] {
+  return HEROES;
+}
+}
